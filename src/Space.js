e66f24c69736a1ac45ad2b1337ed83570a44d9bb
Ro Malik <49294844+CilantroMalik@users.noreply.github.com>
2023-08-14 12:10:41 -0400

    Finish space page

--- src/Space.js
+++ src/Space.js
@@ -57,10 +57,24 @@ export const Space = () => {
             <h1 style={{fontSize: isMobile ? "9vw" : "5rem", fontWeight: "900"}}>———&nbsp;&nbsp;Storybook&nbsp;&nbsp;———</h1>
             <div style={{fontSize: "1.5rem"}}><em style={{fontWeight: "800", marginBottom: "1rem"}}>Hi, {location.state ? location.state.user : ""}!</em>&nbsp;&nbsp;&nbsp;&nbsp;[ <span onMouseEnter={() => setHoveringLogout(true)} onMouseLeave={() => setHoveringLogout(false)} style={{fontWeight: hoveringLogout ? "900" : "300"}} onClick={() => navigate("/welcome")}>logout</span> ]</div>
             <hr style={{width: "95%", color: "#f1f7ed"}}/>
-            <div style={{display: "flex", flexDirection: "row", justifyContent: "center", alignItems: "center"}}><h1><strong>Current space: {spaceData.name} (Join code {spaceData.code})</strong></h1>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<h2 style={{margin: "1rem 0", fontWeight: "300"}}>[ <span onMouseEnter={() => setHoveringReturn(true)} onMouseLeave={() => setHoveringReturn(false)} style={{fontWeight: hoveringReturn ? "900" : "300"}} onClick={() => navigate("/", {state: {user: location.state.user}})}>return home</span> ]</h2></div>
-            <h5 style={{margin: "0", fontWeight: "900"}}>Members: <span style={{fontWeight: "300"}}>{spaceData.members === [] ? "Loading..." : spaceData.members.join(", ")}</span></h5>
-            <h4><em>{spaceData.storyLength !== 40 ? <>Story in progress... it's {spaceData.whoseTurn}'s turn.</> : <>Story complete!</>}</em>&nbsp; &nbsp; [ <span style={{color: "#ffffff"}} onClick={refresh}><u>refresh</u></span> ]</h4>
-            <p style={{minHeight: "10rem", width: "75%", border: "2px solid #f1f7ed", borderRadius: "1.5rem", padding: "1rem", margin: "1.5rem 0"}}>{spaceData.story === "" ? <em style={{opacity: "0.5"}}>This story is yet to be written...</em> : <>{spaceData.story}</>}</p>
+            {isMobile ?
+                <div style={{display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "center", margin: "0 1rem"}}>
+                    <h1 style={{margin: "0", textAlign: "center"}}><strong>Current space: {spaceData.name} (Join code {spaceData.code})</strong></h1>
+                    <h3 style={{margin: "0.5rem 0 2rem 0", fontWeight: "300", textAlign: "center"}}>[ <span onMouseEnter={() => setHoveringReturn(true)} onMouseLeave={() => setHoveringReturn(false)} style={{fontWeight: hoveringReturn ? "900" : "300"}} onClick={() => navigate("/", {state: {user: location.state.user}})}>return home</span> ]</h3>
+                </div>
+                :
+                <div style={{display: "flex", flexDirection: "row", justifyContent: "center", alignItems: "center"}}><h1><strong>Current space: {spaceData.name} (Join code {spaceData.code})</strong></h1>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<h2 style={{margin: "1rem 0", fontWeight: "300"}}>[ <span onMouseEnter={() => setHoveringReturn(true)} onMouseLeave={() => setHoveringReturn(false)} style={{fontWeight: hoveringReturn ? "900" : "300"}} onClick={() => navigate("/", {state: {user: location.state.user}})}>return home</span> ]</h2></div>
+            }
+            <h5 style={{margin: "0 1rem", fontWeight: "900", textAlign: "center"}}>Members: <span style={{fontWeight: "300"}}>{spaceData.members === [] ? "Loading..." : spaceData.members.join(", ")}</span></h5>
+            { isMobile ?
+                <div style={{display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "center", margin: "0 1rem"}}>
+                    <h4 style={{margin: "1rem 0 0 0"}}><em>{spaceData.storyLength !== 40 ? <>Story in progress... it's {spaceData.whoseTurn}'s turn.</> : <>Story complete!</>}</em></h4>
+                    <h4 style={{margin: "0.5rem 0 1rem 0"}}>[ <span style={{color: "#ffffff"}} onClick={refresh}><u>refresh</u></span> ]</h4>
+                </div>
+                :
+                <h4><em>{spaceData.storyLength !== 40 ? <>Story in progress... it's {spaceData.whoseTurn}'s turn.</> : <>Story complete!</>}</em>&nbsp; &nbsp; [ <span style={{color: "#ffffff"}} onClick={refresh}><u>refresh</u></span> ]</h4>
+            }
+            <p style={{minHeight: "10rem", width: "90%", border: "2px solid #f1f7ed", borderRadius: "1rem", padding: "1rem", margin: "1.5rem 0"}}>{spaceData.story === "" ? <em style={{opacity: "0.5"}}>This story is yet to be written...</em> : <>{spaceData.story}</>}</p>
             {location.state && spaceData.whoseTurn === location.state.user ?
                 <div style={{display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "center", marginTop: "1rem", width: "90%"}}>
                     <textarea placeholder="Add a sentence here. Be thoughtful, but spontaneous :)" style={{width: "100%", color: "#f1f7ed", marginBottom: "1rem"}} value={sentence} onChange={e => setSentence(e.target.value)}/>
