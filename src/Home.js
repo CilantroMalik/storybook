4617acf35ee751cd3d27814531898ceb07f9b256
Ro Malik <49294844+CilantroMalik@users.noreply.github.com>
2023-08-15 22:07:10 -0400

    Make it possible for owner to delete a space

--- src/Home.js
+++ src/Home.js
@@ -116,6 +116,24 @@ export const Home = () => {
         })
     }
 
+    const deleteSpace = (spaceObj) => {
+        fetch("https://cilantroleaf.space/strbk/v1/delete-space", {
+            method: "POST",
+            mode: "cors",
+            headers: {
+                "Access-Control-Allow-Origin": "*",
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({
+                username: location.state.user,
+                spaceCode: spaceObj.code
+            })
+        }).then(res => res.json()).then(data => {
+            console.log(data)
+            setUserSpaces(data.userSpaces)
+        })
+    }
+
     return (
         <div style={{display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "center", fontFamily: "IBM Plex Serif"}}>
             <h1 style={{fontSize: isMobile ? "9vw" : "5rem", fontWeight: "900"}}>———&nbsp;&nbsp;Storybook&nbsp;&nbsp;———</h1>
@@ -173,6 +191,7 @@ export const Home = () => {
                         <div style={{backgroundColor: "#a9a9a9", width: "2px", height: "100%", margin: "0 2rem"}}>&nbsp;</div>
                         <button className="muted-button" style={{marginRight: "2rem"}} onClick={() => navigate("/space", {state: {user: location.state.user, data: spaceObj}})}>Enter</button>
                         {spaceObj.members.length !== 1 && <button className="muted-button" style={{marginRight: "2rem"}} onClick={() => leaveSpace(spaceObj)}>Leave Space</button>}
+                        {location.state && location.state.user === spaceObj.owner && <button className="muted-button" style={{marginRight: "2rem", borderColor: "#ee9a9a", color: "#ee9a9a"}} onClick={() => deleteSpace(spaceObj)}>Delete Space</button>}
                     </div>
                 }</>
             })}
